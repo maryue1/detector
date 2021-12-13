6f62559396828f539c74d65915f8ccfeb16b279c
maryue1 <230448@edu.p.lodz.pl>
2021-12-13 18:30:36 +0100

    Changes connected with creating popup

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,187 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<main>
+<div class="page-container">
+    <div class="input-container">
+        <h1 data-highlightword="FAKE ">NEWS DETECTOR</h1>
+        <span>Insert news to check:</span>
+        <br>
+        <br>
+        <span class="textarea">
+            <textarea v-model="message" placeholder="add multiple lines" rows="15" cols="80"></textarea>
+        </span>
+        <br>
+        <button class="popup-open">Check</button>
+        <Popup v-if="popupTriggers.buttonTrigger">
+          <h2>Results</h2>
+        </Popup>
+    </div>
+</div>
+</main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref } from 'vue';
+import Popup from './components/Popup.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup () {
+    const popupTriggers = ref({
+      buttonTrigger: false
+    });
+    return {
+      Popup,
+      popupTriggers
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+    width: 100%;
+    min-height: 100%;
+    overflow: auto;
 }
+
+
+body {
+    width: 100%;
+    min-height: 100vh;
+    overflow: auto;
+}
+
+#app {
+    font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #FDF5E6;
+    letter-spacing: .05em;
+    text-shadow: 
+      0 -1px 0 #fff, 
+      1px 1px 0 #2e2e2e, 
+      2px 2px 0 #2c2c2c, 
+      3px 3px 0 #2a2a2a, 
+      4px 4px 0 #282828, 
+      5px 5px 0 #262626, 
+      6px 6px 0 #242424, 
+      7px 7px 0 #222, 
+      8px 8px 0 #202020, 
+      0 22px 30px rgba(0, 0, 0, 0.9);
+    background-color: #2e2e2e;
+    width: 100%;
+    min-height: 100vh;
+}
+
+.page-container {
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+}
+h1[data-highlightword] {
+    font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #FDF5E6;
+    margin-top: 20px;
+    letter-spacing: .005em;
+    text-shadow:
+        1px 1px 0 #2e2e2e,
+        2px 2px 0 #2c2c2c,
+        3px 3px 0 #2a2a2a,
+        4px 4px 0 #282828,
+        5px 5px 0 #262626,
+        6px 6px 0 #242424,
+        7px 7px 0 #222,
+        8px 8px 0 #202020,
+        0 22px 30px rgba(0, 0, 0, 0.9);
+}
+
+h1[data-highlightword]::before {
+    content: attr(data-highlightword);
+    color: #b30000;
+
+}
+
+button {
+    color: #fff;
+    justify-content: center;
+    align-items: center;
+    padding: 0 20px;
+    padding-top: 0px;
+    padding-right: 20px;
+    padding-bottom: 0px;
+    padding-left: 20px;
+    min-width: 160px;
+    height: 62px;
+    background-color: transparent;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
+    font-size: 16px;
+    line-height: 1.2;
+    text-transform: uppercase;
+    -webkit-transition: all 0.4s;
+    -o-transition: all 0.4s;
+    -moz-transition: all 0.4s;
+    transition: all 0.4s;
+    z-index: 1;
+    position: relative;
+    border-width: 8px;
+    border-color: #1beabd;
+}
+
+textarea {
+    position: relative;
+    font-size: 10px;
+    color: white;
+    background: #404040;
+    padding: 15px;
+    border-width: 4px;
+    border-color: #1beabd;
+}
+
+::placeholder {
+    color: #f2f2f2;
+    font-size: 14px;
+}
+
+.input-container {
+    font-family: "Avant Garde", Avantgarde, "Century Gothic", CenturyGothic, "AppleGothic", sans-serif;
+    font-size: 40px;
+    text-align: center;
+    text-transform: uppercase;
+    text-rendering: optimizeLegibility;
+    background-color: #696969;
+    padding: 20px;
+    text-shadow:
+        0 -1px 0 #fff,
+        1px 1px 0 #2e2e2e,
+        2px 2px 0 #2c2c2c,
+        3px 3px 0 #2a2a2a,
+        4px 4px 0 #282828,
+        5px 5px 0 #262626,
+        0 22px 30px rgba(0, 0, 0, 0.9);
+}
+
+.popup {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0,0,0,0.2);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.popup-inner {
+  background: #FFF;
+  padding: 32px;
+}
+
 </style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="popup">
+        <div class="popup-inner">
+            <slot />
+            <button class="popup-close">
+                Close
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+
+}
+    
+</script>
+
+<style >
+
+</style>
